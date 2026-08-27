8d5eb83ab7a5d6e9c144845be470b77916eb5990
SULTANUM MOBIN <sultanum.mobin@gmail.com>
2026-08-28 00:49:43 +0600

    optimize code instade of two diffrent return

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,15 +2,10 @@ function ListGroup() {
     let area = ['Dhaka', 'Rangpur', 'Barishal', 'Rajshahi', 'Khulna', 'Sylhet', 'Mymenshingh'];
 
     area =[];
-    if(area.length == 0){
-        return <>
-        <h1>List Items</h1>
-        <p>No Item Found</p>
-        </>
-    }
 
     return <>
         <h1>List Items</h1>
+        {area.length == 0 && <p>No Item Found</p>}
         <div className="list-group">
             {area.map(items => <a key={items} href="#" className="list-group-item list-group-item-action">{items}</a>)}
         </div>
