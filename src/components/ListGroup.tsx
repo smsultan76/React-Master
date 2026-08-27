0ff464d42d3154169b6315e23182a461f15f6b1c
SULTANUM MOBIN <sultanum.mobin@gmail.com>
2026-08-28 00:42:45 +0600

    fixed key proms error

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,10 +1,18 @@
 function ListGroup() {
-    const area = ['Dhaka', 'Rangpur', 'Barishal', 'Rajshahi', 'Khulna', 'Sylhet', 'Mymenshingh'];
+    let area = ['Dhaka', 'Rangpur', 'Barishal', 'Rajshahi', 'Khulna', 'Sylhet', 'Mymenshingh'];
+
+    area =[];
+    if(area.length == 0){
+        return <>
+        <h1>List Items</h1>
+        <p>No Item Found</p>
+        </>
+    }
 
     return <>
         <h1>List Items</h1>
         <div className="list-group">
-            {area.map(items => <a href="#" className="list-group-item list-group-item-action">{items}</a>)}
+            {area.map(items => <a key={items} href="#" className="list-group-item list-group-item-action">{items}</a>)}
         </div>
     </>;
 }
