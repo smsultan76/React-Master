e38d27ea3852880f4c3114e91129c2d73f27167a
SULTANUM MOBIN <sultanum.mobin@gmail.com>
2026-08-28 01:14:15 +0600

    catch the event

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,20 @@
+import type { MouseEvent } from "react";
 function ListGroup() {
     let area = ['Dhaka', 'Rangpur', 'Barishal', 'Rajshahi', 'Khulna', 'Sylhet', 'Mymenshingh'];
 
-    area =[];
-
+    const handleCLick = (event: MouseEvent<HTMLAnchorElement>) => {
+        event.preventDefault;
+        console.log(event)
+    };
     return <>
         <h1>List Items</h1>
         {area.length == 0 && <p>No Item Found</p>}
         <div className="list-group">
-            {area.map(items => <a key={items} href="#" className="list-group-item list-group-item-action">{items}</a>)}
+            {area.map((items) => (
+                <a key={items} href="#"
+                    className="list-group-item list-group-item-action"
+                    onClick={handleCLick}>
+                </a>))}
         </div>
     </>;
 }
