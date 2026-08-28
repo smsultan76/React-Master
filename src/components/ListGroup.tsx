6b3516ff7f3d8fcdba9ea583974716bbcc95cc3a
SULTANUM MOBIN <sultanum.mobin@gmail.com>
2026-08-29 00:03:58 +0600

    dynamicaly select items

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,10 @@
-import type { MouseEvent } from "react";
+
 function ListGroup() {
     let area = ['Dhaka', 'Rangpur', 'Barishal', 'Rajshahi', 'Khulna', 'Sylhet', 'Mymenshingh'];
 
-    const select = 0;
+    let select = 0;
     const disable = 3;
-    const handleCLick = (event: MouseEvent<HTMLAnchorElement>) => {
-        event.preventDefault;
-        console.log(event)
-    };
+
     return <>
         <h1>List Items</h1>
         {area.length == 0 && <p>No Item Found</p>}
@@ -15,7 +12,7 @@ function ListGroup() {
             {area.map((items, index) => (
                 <a key={items} href="#"
                     className={select == index ? 'list-group-item list-group-item-action active' : disable == index ? 'list-group-item list-group-item-action disabled' : 'list-group-item list-group-item-action'}
-                    onClick={handleCLick}>{items}
+                    onClick={() => {select = index;}}>{items}
                 </a>
             ))}
         </div>
