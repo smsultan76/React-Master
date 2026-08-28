2e12814bb32104c1c8f766c7bac1bf34864a4db3
SULTANUM MOBIN <sultanum.mobin@gmail.com>
2026-08-29 00:12:35 +0600

    use of useState

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,10 +1,10 @@
+import { useState } from "react";
 
 function ListGroup() {
     let area = ['Dhaka', 'Rangpur', 'Barishal', 'Rajshahi', 'Khulna', 'Sylhet', 'Mymenshingh'];
 
-    let select = 0;
     const disable = 3;
-
+    const [select, setSelect] = useState(-1);
     return <>
         <h1>List Items</h1>
         {area.length == 0 && <p>No Item Found</p>}
@@ -12,7 +12,7 @@ function ListGroup() {
             {area.map((items, index) => (
                 <a key={items} href="#"
                     className={select == index ? 'list-group-item list-group-item-action active' : disable == index ? 'list-group-item list-group-item-action disabled' : 'list-group-item list-group-item-action'}
-                    onClick={() => {select = index;}}>{items}
+                    onClick={() => {setSelect(index)}}>{items}
                 </a>
             ))}
         </div>
