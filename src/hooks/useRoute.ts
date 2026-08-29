2c94ad9860aaef169f16c5611972f8ac569b3ac9
SULTANUM MOBIN <sultanum.mobin@gmail.com>
2026-08-29 23:37:00 +0600

    reload after url change

--- src/hooks/useRoute.ts
+++ src/hooks/useRoute.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function useRouter() {
     const [path, setPath] = useState(window.location.pathname);
@@ -6,7 +6,17 @@ function useRouter() {
     const navigate = (url: string) => {
         window.history.pushState({}, "", url);
         setPath(url);
+        window.location.reload();
     }
+    useEffect(() => {
+        const handlePopState = () => {
+            setPath(window.location.pathname);
+        }
+        window.addEventListener("popstate", handlePopState)
+        return () => {
+            window.removeEventListener("popstate", handlePopState);
+        }
+    }, []);
     return { path, navigate };
 }
 
